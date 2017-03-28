ed142dd62e0cc95aae5bd87373dd8e9a2f21de3d
Charles Cao <caotaotao@gmail.com>
2017-03-28 17:08:43 -0400

    add the pairwise testing framework

--- src/test/java/com/ecvictor/api/rest/assured/o2o/PairwiseRestAssuredRest.java
+++ src/test/java/com/ecvictor/api/rest/assured/o2o/PairwiseRestAssuredRest.java
@@ -1,19 +1,18 @@
-package com.ecvictor.api.pairwise;
+package com.ecvictor.api.rest.assured.o2o;
 
 import org.junit.Test;
 
 /**
  * Created by caoc on 3/25/2017.
  */
-public class PairwiseSample {
+public class PairwiseRestAssuredRest {
     @Test
     public void buildTestSets() {
 //        String NAV_SCENARIO =
 //                "Browser: Chrome, Firefox, InternetExplorer, Safari"
-//                        + "\nPage: Home, Category, Search, New Products"
+//                        + "\nPage: Home, Category, Search, NewProducts"
 ////                        + "\nProduct: Phone, Movie, Computer, Blender, Microwave, Book, Sweater"
-//                        + "\nClick: Link, Image, Description"
-//                ;
+//                        + "\nClick: Link, Image, Description";
 //
 //        //First, generate the list of vectors we *want*
 //        IInventory inventory = PairwiseInventoryFactory.generateParameterInventory(NAV_SCENARIO);
@@ -22,8 +21,16 @@ public class PairwiseSample {
 //        //Now, go through the vectors in the database to figure out what we already *have*
 //        // If we don't have it already, create it
 //        int index = 0;
-//        for (Map<String, String> rawTestCase: rawDataSet) {
+//        for (Map<String, String> rawTestCase : rawDataSet) {
 //            System.out.println(String.format("Test Case %03d: [%s] [%s] [%s] [%s]", index++, rawTestCase.get("Browser"), rawTestCase.get("Page"), rawTestCase.get("Product"), rawTestCase.get("Click")));
+//
+//            expect().statusCode(200);
+//            expect().body("browser", equalTo(rawTestCase.get("Browser")))
+//                    .body("page", equalTo(rawTestCase.get("Page")))
+//                    .contentType("application/json; charset=ISO-8859-1")
+//                    .log().all()
+//                    .given()
+//                    .when().get("http://echo.jsontest.com/browser/" + rawTestCase.get("Browser") + "/page/" + rawTestCase.get("Page"));
 //        }
     }
 }
