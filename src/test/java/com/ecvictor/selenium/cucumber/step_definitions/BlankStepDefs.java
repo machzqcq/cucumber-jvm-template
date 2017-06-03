65eb512b5715d49102984a555fbc47df2dc1bbc9
ccao <ccao@ecvictor.com>
2017-06-03 16:36:22 -0400

    Move hook to def

--- src/test/java/com/ecvictor/selenium/cucumber/step_definitions/BlankStepDefs.java
+++ src/test/java/com/ecvictor/selenium/cucumber/step_definitions/BlankStepDefs.java
@@ -10,11 +10,12 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class BlankStepDefs {
-    public WebDriver driver = Hooks.driver;
+public class BlankStepDefs{
+    public WebDriver driver;
 
-    public BlankStepDefs() {
-        Log.startTestCase(BlankStepDefs.class.getName());
+    public BlankStepDefs()
+    {
+    	driver = Hooks.driver;
     }
 
     @When("^I open seleniumframework website$")
@@ -22,7 +23,6 @@ public class BlankStepDefs {
         // Write code here that turns the phrase above into concrete actions
         driver.get("http://www.seleniumframework.com");
     }
-
 
     @Then("^I validate title and URL$")
     public void i_print_title_and_URL() throws Throwable {
